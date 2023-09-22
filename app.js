8a2f71419df8627cfcfb1db1dceef202ddd769bc
Hasan Al <Hasanalghanim@gmail.com>
2023-09-22 11:26:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -19,18 +19,8 @@ var allowedOrigins = [
 ];
 app.use(
 	cors({
-		origin: function (origin, callback) {
-			// allow requests with no origin
-			// (like mobile apps or curl requests)
-			if (!origin) return callback(null, true);
-			if (allowedOrigins.indexOf(origin) === -1) {
-				const msg =
-					'The CORS policy for this site does not ' +
-					'allow access from the specified Origin.';
-				return callback(new Error(msg), false);
-			}
-			return callback(null, true);
-		},
+		origin: allowedOrigins,
+		methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
 	})
 );
 
