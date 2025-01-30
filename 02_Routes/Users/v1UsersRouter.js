68ac3e7eeeced59651194a00005ef5d209416690
ysanroterra <ysanchez@roterra.com>
2025-01-30 10:26:04 -0700

    added v1 userscontroller + getusers endpoint

--- 02_Routes/Users/v1UsersRouter.js
+++ 02_Routes/Users/v1UsersRouter.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const authorize = require('../Authorization/authorization');
+
 const usersDashboardRouter = require('./usersDashboard/usersDashboardRouter');
 const usersBoardingRouter = require('./usersBoarding/usersBoardingRouter');
 const usersInformationRouter = require('./usersInformation/usersInformationRouter');
 const usersHierarchyRouter = require('./usersHierarchy/usersHierarchyRouter');
 
+const usersController = require('./usersController')
+
 router.use('/dashboard', usersDashboardRouter);
 router.use('/usersBoarding', usersBoardingRouter);
 router.use('/usersInformation', usersInformationRouter);
 router.use('/usersHierarchy', usersHierarchyRouter);
+
+router.get('', authorize({}), usersController.getUsers)
 
 module.exports = router;
