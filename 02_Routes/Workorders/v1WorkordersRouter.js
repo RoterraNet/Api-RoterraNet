08e2bfdda93262407d3bc59adc59be674f164b48
ysanroterra <ysanchez@roterra.com>
2025-04-02 10:03:12 -0600

    added v1 get workorders endpoint

--- 02_Routes/Workorders/v1WorkordersRouter.js
+++ 02_Routes/Workorders/v1WorkordersRouter.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const authorize = require('../Authorization/authorization');
 
 const workorderHeatsController = require('./workorderHeatsController');
+const workordersController = require('./workordersController');
+
+router.get('', authorize({}), workordersController.getWorkorders);
 
 router.get('/heats', authorize({}), workorderHeatsController.getWorkorderHeats);
 
