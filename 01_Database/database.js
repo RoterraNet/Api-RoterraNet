51b4448489037c7af11d43b44172622c6da4282b
ysanroterra <ysanchez@roterra.com>
2025-04-07 15:47:36 -0600

    added personal property get/post tables

--- 01_Database/database.js
+++ 01_Database/database.js
@@ -117,6 +117,9 @@ exports.postNewsDB = 'roterranet.news';
 exports.getNotesDB = 'roterranet.view_notes';
 exports.postNotesDB = 'roterranet.notes';
 
+exports.getPersonalPropertyDB = 'roterranet.personal_property';
+exports.postPersonalPropertyDB = 'roterranet.view_personal_property';
+
 exports.getPipesDB = 'roterranet.pipe'; //change DB name to pipes
 exports.postPipesDB = 'roterranet.pipe'; //change DB name to pipes
 
