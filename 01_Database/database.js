279595ac1ad6e3e33c376de2321379a97c241b97
ysanroterra <ysanchez@roterra.com>
2025-04-08 13:03:40 -0600

    swapped get/post tables

--- 01_Database/database.js
+++ 01_Database/database.js
@@ -117,8 +117,8 @@ exports.postNewsDB = 'roterranet.news';
 exports.getNotesDB = 'roterranet.view_notes';
 exports.postNotesDB = 'roterranet.notes';
 
-exports.getPersonalPropertyDB = 'roterranet.personal_property';
-exports.postPersonalPropertyDB = 'roterranet.view_personal_property';
+exports.getPersonalPropertyDB = 'roterranet.view_personal_property';
+exports.postPersonalPropertyDB = 'roterranet.personal_property';
 
 exports.getPipesDB = 'roterranet.pipe'; //change DB name to pipes
 exports.postPipesDB = 'roterranet.pipe'; //change DB name to pipes
