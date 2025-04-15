322113fce1517d6cb880c34786224d5da80fc687
ysanroterra <ysanchez@roterra.com>
2025-04-15 12:24:11 -0600

    removed unused newhires controller

--- 02_Routes/Users/usersDashboard/usersDashboardRouter.js
+++ 02_Routes/Users/usersDashboard/usersDashboardRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const authorize = require('../../Authorization/authorization');
 const router = express.Router();
-const userDashboardController = require('./newHiresController');
+
 const offBoardingController = require('./offBoardingController');
 const HrTodoController = require('./HrTodoController');
 const onboardingTodosController = require('./hrDashboard/onboardingTodosController');
@@ -28,79 +28,6 @@ router.put(
 	authorize({}),
 	testOffboardingTodosController.makeOffboardingTodos
 );
-
-/**
- * @openapi
- * /api/v1/users/dashboard/newHires:
- *   get:
- *     summary: Retrieve all new hires
- *     tags:
- *       - Users New Hires
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - name: Authorization
- *         in: header
- *         description: JWT token for authorization
- *         required: true
- *     responses:
- *       '200':
- *         description: A list of all new hires
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/NewHire'
- *     securitySchemes:
- *       bearer:
- *         type: http
- *         scheme: bearer
- *         bearerFormat: JWT
- */
-router.get('/newHires', authorize({}), userDashboardController.getAllNewHires);
-
-/**
- * @openapi
- * /api/v1/users/dashboard/newHires:
- *   put:
- *     summary: Update a new hire
- *     tags:
- *       - Users New Hires
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - name: Authorization
- *         in: header
- *         description: JWT token for authorization
- *         required: true
- *         schema:
- *           type: string
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/UpdateNewHire'
- *     responses:
- *       '200':
- *         description: A list of all new hires
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/NewHire'
- *     securitySchemes:
- *       bearerAuth:
- *         type: http
- *         scheme: bearer
- *         bearerFormat: JWT
- *         description: |
- *           JWT token for authorization.
- *           You can obtain a token by logging in and using the /api/auth/login endpoint.
- */
-router.put('/newHires', authorize({}), userDashboardController.updateOneNewHireCheck);
 
 /**
  * @openapi
