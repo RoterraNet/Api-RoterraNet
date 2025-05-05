f116745902f7c4f280dcfbd387e5cd53ce9262b0
Hasan Al <Hasanalghanim@gmail.com>
2025-05-05 13:55:06 -0600

    Update Table export endpoint

--- 02_Routes/Workorders/WorkorderStats/WorkorderStats.js
+++ 02_Routes/Workorders/WorkorderStats/WorkorderStats.js
@@ -5,6 +5,7 @@ const knex = require('../../../01_Database/connection');
 const { format } = require('date-fns');
 const SearchBuilder = require('../../RouteCreaters/RouteHelpers/SearchBuilder');
 const getWorkordersItemsDetailsDB = database.getWorkordersItemsDetailsDB;
+const XLSX = require('xlsx');
 
 router.get(`/table`, async (req, res) => {
 	const { start, size, filters, sorting, globalFilter } = req.query;
@@ -108,3 +109,84 @@ router.get('/stats/', async (req, res) => {
 });
 
 module.exports = router;
+
+router.get('/table/download', async (req, res) => {
+	const { filters } = req.query;
+
+	try {
+		const parsedColumnFilters = JSON.parse(filters);
+		const paginatedTable = await knex(getWorkordersItemsDetailsDB)
+			.select(
+				'workorder_item_detail_id',
+				'workorder_item_detail_name',
+				'pipe_approved_on',
+				'pipe_approved_name',
+				'pipe_od',
+				'pipe_wall',
+				'pipe_length',
+				'helix_1_thickness',
+				'helix_2_thickness',
+				'helix_3_thickness',
+				'helix_4_thickness',
+				'helix_1_diameter',
+				'helix_2_diameter',
+				'helix_3_diameter',
+				'helix_4_diameter',
+				'workorder_item_description'
+			)
+			.whereNotNull('pipe_approved_on')
+			.orderBy('workorder_item_detail_id', 'desc')
+			.modify((builder) => {
+				if (!!parsedColumnFilters?.length) {
+					parsedColumnFilters.map((filter) => {
+						const { id: columnId, value: filterValue } = filter;
+						if (columnId === 'pipe_approved_on') {
+							if (!filterValue?.start && !filterValue?.finish) {
+							} else if (!filterValue?.start) {
+								builder.where(
+									columnId,
+									'<=',
+									`"${format(new Date(filterValue?.finish), 'yyyy-MM-dd')}"`
+								);
+							} else if (!filterValue?.finish) {
+								builder.where(
+									columnId,
+									'>=',
+									`"${format(new Date(filterValue?.start), 'yyyy-MM-dd')}"`
+								);
+							} else {
+								builder.whereBetween(columnId, [
+									`"${format(
+										new Date(filterValue?.start),
+										'yyyy-MM-dd'
+									)} 00:00:00"`,
+									`"${format(
+										new Date(filterValue?.finish),
+										'yyyy-MM-dd'
+									)} 20:00:00"`,
+								]);
+							}
+						} else {
+							builder.whereRaw(`${columnId}::text iLIKE ?`, [`%${filterValue}%`]);
+						}
+					});
+				}
+			})
+			.orderBy('pipe_approved_on', 'desc');
+
+		const workSheet = XLSX.utils.json_to_sheet(paginatedTable);
+		const workBook = XLSX.utils.book_new();
+		XLSX.utils.book_append_sheet(workBook, workSheet, 'Data');
+
+		const wbout = XLSX.write(workBook, { bookType: 'xlsx', type: 'buffer' });
+
+		res.setHeader('Content-Disposition', 'attachment; filename="Data.xlsx"');
+		res.setHeader(
+			'Content-Type',
+			'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+		);
+		res.send(Buffer.from(wbout));
+	} catch (error) {
+		console.log(error);
+	}
+});
