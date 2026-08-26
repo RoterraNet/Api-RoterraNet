2ecbae7ba122cfcc0a976146dfaa099e8c9c09ca
ysanroterra <ysanchez@roterra.com>
2026-08-26 10:42:33 -0600

    added registration/invites routers to v1UsersRouter

--- 02_Routes/Users/v1UsersRouter.js
+++ 02_Routes/Users/v1UsersRouter.js
@@ -5,12 +5,16 @@ const authorize = require('../Authorization/authorization');
 const usersDashboardRouter = require('./usersDashboard/usersDashboardRouter');
 const usersBoardingRouter = require('./usersBoarding/usersBoardingRouter');
 const usersInformationRouter = require('./usersInformation/usersInformationRouter');
+const userInvitesRouter = require('./userInvites/userInvitesRouter');
+const userRegistrationRouter = require('./userRegistration/userRegistrationRouter');
 
 const usersController = require('./usersController');
 
 router.use('/dashboard', usersDashboardRouter);
 router.use('/usersBoarding', usersBoardingRouter);
 router.use('/usersInformation', usersInformationRouter);
+router.use('/userInvites', userInvitesRouter);
+router.use('/userRegistration', userRegistrationRouter);
 
 router.get('', authorize({}), usersController.getUsers);
 router.put('', authorize({}), usersController.updateUser);
